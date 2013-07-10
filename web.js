875f7eee9c393031e14c79ab77fc3cd55add534c
Carlo Sorrentino <sig.carlo@gmail.com>
2013-07-10 18:54:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var buffertext = new Buffer(fs.readFileSync('index.html'));
-var text = buffertext.toString;
+var text = buffertext.toString("utf-8");
 
 app.get('/', function(request, response) {
   response.send(text);
